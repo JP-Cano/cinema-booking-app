14fa32900f159dd11564056a0726dbb06cb0d1bf
Juan Pablo Cano <canojuan0415@gmail.com>
2023-03-27 16:46:42 -0500

    Added swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './application/modules/app.module';
-import { UTIL } from './application/utils/enums/Util';
+import { AppModule } from './modules/app.module';
+import { UTIL } from './utils/enums/Util';
 
 const PORT = process.env.PORT || UTIL.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      //whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(PORT, () =>
     Logger.log(`Open server on http://localhost:${PORT}`),
   );
